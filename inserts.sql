2458b7e096ffdbbf526ac16f8ee4215e3e4057e8
fignaomi <naomi.figueiredo@gmail.com>
2022-06-02 00:20:48 -0300

    inserts

---
+++ inserts.sql
@@ -0,0 +1,49 @@
+insert into oleo (nome_comum, nome_cientifico, foto, descricao)
+values("Alecrim", "Rosmarinus officinalis L.",
+ "https://firebasestorage.googleapis.com/v0/b/aroma-5ff99.appspot.com/o/alecrim.jpg?alt=media&token=828de97c-bb75-4a76-95c8-a36f084dac0b",
+ "Antiespasmódico, carminativo, cefálico, descongestionante, digestivo, estimulante, e sudorífico.");
+
+insert into oleo (nome_comum, nome_cientifico, foto, descricao)
+values("Camomila-romana", "Chamaemelum nobile",
+ "https://firebasestorage.googleapis.com/v0/b/aroma-5ff99.appspot.com/o/Camomila.jpg?alt=media&token=e93d7edb-bdca-4f50-90bd-e0c32af44fcd",
+ "Analgésico, anti-inflamatório, cicatrizante, remédio hepático e sedativo.");
+ 
+insert into oleo (nome_comum, nome_cientifico, foto, descricao)
+values("Eucalipto", "Eucalyptus globulus Labill",
+ "https://firebasestorage.googleapis.com/v0/b/aroma-5ff99.appspot.com/o/Eucalipto.jpg?alt=media&token=c14c213b-d967-4a4e-a191-09bab23056cf",
+ "Analgésico, anti virótico, expectorante e estimulante");
+
+insert into oleo (nome_comum, nome_cientifico, foto, descricao)
+values("Lavanda", "Lavandula Officinalis",
+ "https://firebasestorage.googleapis.com/v0/b/aroma-5ff99.appspot.com/o/Lavanda.jpg?alt=media&token=e17c410d-b399-43d6-8e61-d7c6aa0270a4",
+ "Analgésico, antisséptico l, antioxidantes, cicatrizante l e sedativo.");
+
+insert into oleo (nome_comum, nome_cientifico, foto, descricao)
+values("Rosa", "Rosa × centifolia",
+ "https://firebasestorage.googleapis.com/v0/b/aroma-5ff99.appspot.com/o/Rosa.jpg?alt=media&token=dadd6aa3-fb18-4958-a19b-657cca503931",
+ "Afrodisíaco, uterino, sedativo, adstringente, rubefaciente, hemostático, hepático.");
+
+insert into oleo (nome_comum, nome_cientifico, foto, descricao)
+values("Jasmim", "jasminum officinale",
+ "https://firebasestorage.googleapis.com/v0/b/aroma-5ff99.appspot.com/o/Jasmim.jpg?alt=media&token=fc6a76b7-2bdf-43f4-8b45-d5dd795949e5",
+ "Antidepressivo, antiespasmódico, afrodisíaco, galactóforo e sedativo.");
+
+insert into oleo (nome_comum, nome_cientifico, foto, descricao)
+values("Hortelã-pimenta", "Mentha piperita",
+ "https://firebasestorage.googleapis.com/v0/b/aroma-5ff99.appspot.com/o/Hortel%C3%A3-pimenta.jpg?alt=media&token=5c35d7d4-9c42-412b-978d-8f6b7b4acc97",
+ "Antiespasmódico, analgésico, carminativo, expectorante, vasoconstritor e cefálico.");
+
+insert into oleo (nome_comum, nome_cientifico, foto, descricao)
+values("Manjericão", "Ocimum basilicum",
+ "https://firebasestorage.googleapis.com/v0/b/aroma-5ff99.appspot.com/o/Manjeric%C3%A3o.jpg?alt=media&token=12a4b8b3-2e37-45c5-8434-566463d32a4f",
+ "Antiespasmódico, cefálico, estimulante, antidepressivo.");
+
+insert into oleo (nome_comum, nome_cientifico, foto, descricao)
+values("Tea tree", "melaleuca alternifolia",
+ "https://firebasestorage.googleapis.com/v0/b/aroma-5ff99.appspot.com/o/Tea%20tree.jpg?alt=media&token=40d24860-55fe-43b8-995b-aafa600dd9d5",
+ "Anti-séptico, anti bactericida, antibiótico, antifúngico, inmunoestimulante, cicatrizante, expectorante.");
+
+insert into oleo (nome_comum, nome_cientifico, foto, descricao)
+values("Salvia", "Salvia officinalis L",
+ "https://firebasestorage.googleapis.com/v0/b/aroma-5ff99.appspot.com/o/Salvia.jpg?alt=media&token=07309622-8f58-4628-bb18-7ae7db2f1f18",
+ "Anti-séptico, antiinflamatório, ansiedade, tensão pré-menstrual, sintomas do climatério.");
