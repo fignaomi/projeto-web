bc02e9a173ffc04f584f6e93378d848eab9ff85d
fignaomi <naomi.figueiredo@gmail.com>
2022-06-21 23:21:35 -0300

    nova versão

--- config/server.js
+++ config/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const expressSession = require('express-session');
+// const expressSession = require('express-session');
+var session = require('cookie-session');
 const consign = require('consign');
 
 const port = process.env.PORT || 3000;
@@ -10,7 +11,7 @@ app.set('views','./app/views');
 
 app.use(express.static('public')) //Define em qual pasta estarão os arquivos estáticos.
 
-app.use(expressSession({
+app.use(session({
         secret: 'sorriu', //Segredo que pode ser qq string
         resave: false, //Regrava do lado do servidor toda vez
         saveUninitialized: false //cria uma sessão nova toda vez
