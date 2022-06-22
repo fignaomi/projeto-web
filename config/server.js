6962e653519c798692382f8478a90915c2069daf
fignaomi <naomi.figueiredo@gmail.com>
2022-06-21 23:36:46 -0300

    make it better

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 var session = require('cookie-session');
 const consign = require('consign');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; 
 const app = express();
 
 app.set('view engine', 'ejs');
